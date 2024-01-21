2a7366210425b0bb69be35b3b73febeb29aa062c
manhattan <andrey2004uor@gmail.com>
2024-01-21 21:31:39 +0200

    fix

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,7 @@ const {isAdmin, isAuth} = require("../middleware/auth.middleware");
 router.post('/create',isAuth,isAdmin,userController.create)
 router.patch('/update/:id',isAuth,isAdmin,userController.update)
 router.delete('/delete/:id',isAuth,isAdmin,userController.delete)
-router.get('/all',userController.getAll)
-router.get('/:id',userController.getOne)
+router.get('/all',isAuth,isAdmin,userController.getAll)
+router.get('/:id',isAuth,isAdmin,userController.getOne)
 
 module.exports = router
